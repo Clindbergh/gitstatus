d7b546caf11ad66cc18264e609c5f08fa0d3931c
Patrick Hodel <patrick.hodel@idh.ch>
2022-08-18 16:36:25 +0200

    Remove newline on prompt

--- gitstatus.prompt.sh
+++ gitstatus.prompt.sh
@@ -100,5 +100,5 @@ shopt -s promptvars
 PS1='\[\033[01;32m\]\u@\h\[\033[00m\] '           # green user@host
 PS1+='\[\033[01;34m\]\w\[\033[00m\]'              # blue current working directory
 PS1+='${GITSTATUS_PROMPT:+ $GITSTATUS_PROMPT}'    # git status (requires promptvars option)
-PS1+='\n\[\033[01;$((31+!$?))m\]\$\[\033[00m\] '  # green/red (success/error) $/# (normal/root)
+PS1+='\[\033[01;$((31+!$?))m\]\$\[\033[00m\] '  # green/red (success/error) $/# (normal/root)
 PS1+='\[\e]0;\u@\h: \w\a\]'                       # terminal title: user@host: dir
